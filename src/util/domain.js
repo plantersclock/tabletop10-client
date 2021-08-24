b132b7441a44bbaeb9a60cb065598d191c280e77
Matthew Wright <mattman861@gmail.com>
2021-08-24 15:31:17 -0400

    remove / from url

--- src/util/domain.js
+++ src/util/domain.js
@@ -1,4 +1,4 @@
 export default process.env.NODE_ENV === "development"
   ? "http://localhost:5000"
   : process.env.NODE_ENV === "production" &&
-    "https://tabletop10-server.herokuapp.com/";
+    "https://tabletop10-server.herokuapp.com";
