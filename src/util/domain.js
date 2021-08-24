6fb30839faecc164d3ab95f359e0c41b8b488829
Matthew Wright <mattman861@gmail.com>
2021-08-24 15:24:56 -0400

    Changed domains to new url

--- src/util/domain.js
+++ src/util/domain.js
@@ -1,4 +1,4 @@
 export default process.env.NODE_ENV === "development"
   ? "http://localhost:5000"
   : process.env.NODE_ENV === "production" &&
-    "https://mern-auth-template-wright.herokuapp.com";
+    "https://tabletop10-server.herokuapp.com/";
