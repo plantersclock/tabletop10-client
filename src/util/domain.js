b6a0990e7450f1fe45d6b3709986bd6da55ee090
Matthew Wright <mattman861@gmail.com>
2021-08-24 11:16:48 -0400

    removed backslash

--- src/util/domain.js
+++ src/util/domain.js
@@ -1,4 +1,4 @@
 export default process.env.NODE_ENV === "development"
   ? "http://localhost:5000"
   : process.env.NODE_ENV === "production" &&
-    "https://mern-auth-template-wright.herokuapp.com/";
+    "https://mern-auth-template-wright.herokuapp.com";
